68b69b1b4f97814269db9efe01e146676a995e88
Varshitha-2002 <varsithaprema@gmail.com>
2026-06-28 21:42:38 +0530

    added new condition

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class ptintOneToHundered
+{
+    public static void Main(String[] args)
+    {
+
+        for (int i = 0; i <= 100; i+=4)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine($"{i} is even");
+            } 
+            Console.WriteLine($"{i} is odd");   
+
+        }
+    }
+    
+}
+          
+
+      
